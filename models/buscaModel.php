1f093e0597002cf9b4b11e45eeea06788e217952
E-studio <rickyurbina@gmail.com>
2019-02-26 08:20:53 -0700

    Lista de propiedades ordenada descendente

--- models/buscaModel.php
+++ models/buscaModel.php
@@ -111,7 +111,7 @@ class buscaModels{
 
 	public function buscaPropiedadesDetalle($tabla){
 
-		$stmt = Conexion::conectar()->prepare("SELECT P.id, P.destacada, P.titulo, P.status, P.precio, P.categoria, P.areaTerreno, P.areaConstruccion, P.habitaciones, P.banos, P.direccion, P.ciudad, P.estado, P.latitud, P.longitud, P.fotoPrincipal, P.fotos, P.detalles, P.caracteristicas, P.vendedor, P.fechaRegistro, U.id as idAgente, U.nombre, U.telefono, U.email, U.password, U.personal, U.titulo as puesto, U.perfil, U.foto, U.estado as activo, U.ultimoLogin, U.fechaNac, U.sociales  FROM $tabla as P, usuarios as U WHERE U.id = P.vendedor");
+		$stmt = Conexion::conectar()->prepare("SELECT P.id, P.destacada, P.titulo, P.status, P.precio, P.categoria, P.areaTerreno, P.areaConstruccion, P.habitaciones, P.banos, P.direccion, P.ciudad, P.estado, P.latitud, P.longitud, P.fotoPrincipal, P.fotos, P.detalles, P.caracteristicas, P.vendedor, P.fechaRegistro, U.id as idAgente, U.nombre, U.telefono, U.email, U.password, U.personal, U.titulo as puesto, U.perfil, U.foto, U.estado as activo, U.ultimoLogin, U.fechaNac, U.sociales  FROM $tabla as P, usuarios as U WHERE U.id = P.vendedor ORDER BY P.fechaRegistro DESC");
 
 		$stmt->bindParam(":propiedad", $propiedad, PDO::PARAM_INT);
 
